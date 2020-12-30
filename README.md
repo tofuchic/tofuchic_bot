3a2129d28bb87cba9a3e5c8dcb7a4a16a9ec1f05
tofuchic <tofu.mental.chicken.heart@gmail.com>
2020-12-30 15:10:00 +0900

    tweepyによるTL読み込みを実装

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 * Twitterの認証情報を記述するファイル
 * [key-sample.py](./key-sample.py)を参考にローカル環境で生成する
     * [.gitignore](./.gitignore)に記述しているためgitで管理されない
+
+## main.py
+* `trigger()`関数に記述したロジックを`python main.py`でトリガーできる
+
+### ToDo
+#### rubyで動かしていた時代のリスペクト
+* とうふぁぼ機能の実装
+* 固定ツイート機能の実装
--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import key
+import tweepy
 
 
 def trigger():
     print('tofuchic_bot triggered')
-    print(key.access_secret)
+    for status in get_n_tweet(50):
+        print(status.text)
+
+
+def get_n_tweet(n):
+    # Getting the key and secret codes from my environment variables
+    consumer_key = key.consumer_key
+    consumer_secret = key.consumer_secret
+    access_token = key.access_token
+    access_secret = key.access_secret
+
+    # Tweepy's process for setting up authorisation
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    api = tweepy.API(auth)
+
+    return tweepy.Cursor(api.home_timeline).items(n)
 
 
 if __name__ == '__main__':
