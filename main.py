a546c8d2641cf17926055359fa893ae34b8cc4f7
tofuchic <tofu.mental.chicken.heart@gmail.com>
2020-12-30 15:14:06 +0900

    不要な変数を削除

--- main.py
+++ main.py
@@ -9,15 +9,9 @@ def trigger():
 
 
 def get_n_tweet(n):
-    # Getting the key and secret codes from my environment variables
-    consumer_key = key.consumer_key
-    consumer_secret = key.consumer_secret
-    access_token = key.access_token
-    access_secret = key.access_secret
-
     # Tweepy's process for setting up authorisation
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_secret)
+    auth = tweepy.OAuthHandler(key.consumer_key, key.consumer_secret)
+    auth.set_access_token(key.access_token, key.access_secret)
     api = tweepy.API(auth)
 
     return tweepy.Cursor(api.home_timeline).items(n)
